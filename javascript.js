71b460fcca0cf7f45322ce578f0f1c2f90985e83
Prashant Ahirrao <142399747+prashantahirrao4@users.noreply.github.com>
2024-01-14 21:35:59 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,102 @@
+const StartBtn = document.querySelector('.start-btn');
+const popupinfo = document.querySelector('.popup-info');
+const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizsection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
+
+StartBtn.onclick = ()=>{
+    popupinfo.classList.add('active');
+    main.classList.add('active');
+}
+exitBtn.onclick = ()=>{
+    popupinfo.classList.remove('active');
+    main.classList.remove('active');
+}
+continueBtn.onclick = ()=>{
+    quizsection.classList.add('active');
+    popupinfo.classList.remove('active');
+    main.classList.remove('active');
+    quizBox.classList.add('active');
+
+    showquestion(0);
+    questioncounter(1);
+    headerScore();
+}
+let questioncount=0;
+let questionNumb=1;
+let userScore = 0;
+
+const nextBtn=document.querySelector('.next-btn');
+
+
+nextBtn.onclick = ()=>{
+    if(questioncount<questions.length -1){
+        questioncount++;
+        showquestion(questioncount);
+
+        questionNumb++;
+        questioncounter(questionNumb);
+        nextBtn.classList.remove('active');
+    }
+    else{
+        console.log('Question Completed');
+    }
+    }
+    
+    const optionlist= document.querySelector('.option-list');
+
+function showquestion(index){
+    const questionText= document.querySelector('.question-text');
+    questionText.textcount='${questions[index].numb}.${questions[index].question}';
+
+    let optionTag = `
+    <div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>
+  `;
+optionlist.innerHTML=optionTag;
+const option = document.querySelectorAll('.option');
+for(let i=0;i<option.length;i++){
+    option[i].setAttribute('onclick','optionSelected(this)');
+}
+}
+function optionSelected(answer){
+    let userAnswer = answer.textContent;
+    let correctAnswer=questions.questioncount.answer;
+    let allOption=optionlist.children.length;
+
+    if(userAnswer == correctAnswer){
+         
+        answer.classList.add('correct');
+        userScore+=1;
+        headerScore();
+    }
+   else{
+    answer.classList.add('incorrect');
+    for(let i=0;i<allOption;i++){
+        if(optionlist.children[i].textContent==correctAnswer){
+            optionlist.children[i].setAttribute('class','option correct');
+        }
+       }
+   }
+
+
+   for(let i=0;i<allOption;i++){
+    optionlist.children[i].classList.add('disabled');
+   }
+   nextBtn.classList.add('active');
+
+}
+
+function questioncounter(index){
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent='${index} of ${questions.length} Question';
+}
+
+function headerScore(){
+    const headerScoreText=document.querySelector('.header-score');
+    headerScoreText.textContent='Score:$(userScore)/$(question.length)';
+}
---
+++ question.js
@@ -0,0 +1,69 @@
+let questions=[
+    {
+      numb:1,
+      question:"What does html for?",
+      answer:"A.Hyper text markup lang",
+      options:[
+         "A.Hyper text markup lang" ,
+         "B.blue cord" ,
+         "C. tower" ,
+         "D.modem" 
+  
+
+
+      ]
+    },
+    {
+        numb:2,
+        question:"The piece of hardware that converts you computers digital signal to an analog signal that can travel over telephone lines is called a ?",
+        answer:"modem",
+        options:[
+         "A.red wire",
+         "B.blue cord", 
+         "C. tower", 
+         "D.modem" 
+  
+        ]
+      },
+
+      {
+        numb:3,
+        question:"The piece of hardware that converts you computers digital signal to an analog signal that can travel over telephone lines is called a ?",
+        answer:"A.red wire",
+        options:[
+         "A.red wire", 
+         "B.blue cord", 
+         "C. tower", 
+         "D.modem" 
+  
+  
+        ]
+      },
+      {
+        numb:4,
+        question:"The piece of hardware that converts you computers digital signal to an analog signal that can travel over telephone lines is called a ?",
+        answer:"B.blue cord",
+        options:[
+         "A.red wire" ,
+         "B.blue cord" ,
+         "C. tower", 
+         "D.modem" 
+  
+  
+        ]
+      },
+      {
+        numb:5,
+        question:"The piece of hardware that converts you computers digital signal to an analog signal that can travel over telephone lines is called a ?",
+        answer:"C. tower",
+        options:[
+            "A.red wire", 
+         "B.blue cord", 
+         "C. tower", 
+         "D.modem" 
+  
+  
+        ]
+      },
+    
+]
